3a6a31279ac4511f92f02446c77d1b017c8bbeb5
SergioHH_22 <sergio.hhidalgo22@gmail.com>
2024-03-13 20:30:13 +0100

    mejoro el test para las sumas

--- test/MainTest.java
+++ test/MainTest.java
@@ -24,13 +24,32 @@ class MainTest {
     @ParameterizedTest(name = "Si la primera cifra es {0} y la segunda es {1}, el resultado es {2} ")
     @CsvSource({"5, 15, 20.0"})
     void pruebaSuma(double sum1, double sum2, double expRresult) {
-        assertEquals(expRresult, Main.sumar(sum1,sum2), 0.1);
+        assertEquals(expRresult, Main.sumar(sum1, sum2), 0.1);
     }
 
 
     @org.junit.jupiter.api.Test
     void restar() {
+        double rest1 = 25.5;
+        double rest2 = 15.0;
+        double expResult = 10.5;
+        double result = Main.restar(rest1, rest2);
+        assertEquals(expResult, result);
+
+        rest1 = 43;
+        rest2 = 22;
+        expResult = 21.0;
+        result = Main.restar(rest1, rest2);
+        assertEquals(expResult, result);
+
     }
+
+    @ParameterizedTest(name = "Si la primera cifra es {0} y la segunda es {1}, el resultado es {2} ")
+    @CsvSource({"96, 13, 83.0"})
+    void pruebaResta(double rest1, double rest2, double expResult) {
+        assertEquals(expResult, Main.restar(rest1, rest2), 0.1);
+    }
+
 
     @org.junit.jupiter.api.Test
     void multiplicar() {
