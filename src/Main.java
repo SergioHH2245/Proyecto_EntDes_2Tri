0096d3921dbe4b9a31ec01cc5eec9e0dbf231f52
SergioHH_22 <sergio.hhidalgo22@gmail.com>
2024-03-13 19:23:23 +0100

    modifico el menu

--- src/Main.java
+++ src/Main.java
@@ -18,23 +18,14 @@ public class Main {
             sc.nextLine();
             switch (opcion) {
                 case "1":
-
+                    sumar();
                     break;
                 case "2":
-
+                    restar();
                     break;
                 case "3":
-
+                    multiplicar();
                     break;
-                case "4":
-
-                    break;
-                case "0":
-                    System.out.println(" -SALIR- ");
-                    System.out.println("¡HASTA LUEGO, USUARIO!");
-                    break;
-                default:
-                    System.out.println("OPCION NO VALIDA");
             }
         } while (!opcion.equals("0"));
 
@@ -50,5 +41,35 @@ public class Main {
         System.out.println("0. SALIR");
         System.out.println(" -------------------------------- ");
         System.out.println("INTRODUCE OPCION: ");
+    }
+
+    public static void sumar() {
+        System.out.println(" -1:SUMAR- ");
+        System.out.println("INTRODUCE LA PRIMERA CIFRA: ");
+        double sum1 = sc.nextDouble();
+        System.out.println("AHORA INTRODUCE LA SEGUNDA CIFRA: ");
+        double sum2 = sc.nextDouble();
+        double suma = sum1 + sum2;
+        System.out.println("EL RESULTADO ES: " + suma);
+    }
+
+    public static void restar() {
+        System.out.println(" -2:RESTAR- ");
+        System.out.println("INTRODUCE LA PRIMERA CIFRA");
+        double rest1 = sc.nextDouble();
+        System.out.println("AHORA INTRODUCE LA CIFRA QUE LE QUIERES RESTAR A LA PRIMERA");
+        double rest2 = sc.nextDouble();
+        double resta = rest1 - rest2;
+        System.out.println("EL RESULTADO ES: " + resta);
+    }
+
+    public static void multiplicar() {
+        System.out.println(" -3: MULTIPLICAR- ");
+        System.out.println("INTRODUCE LA PRIMERA CIFRA");
+        double mult1 = sc.nextDouble();
+        System.out.println("AHORA INTRODUCE LA SEGUNDA CIFRA");
+        double mult2 = sc.nextDouble();
+        double multiplicacion = mult1 * mult2;
+        System.out.println("EL RESULTADO ES: " + multiplicacion);
     }
 }
