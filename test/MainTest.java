dea09a2ffbfa9b7ac7ce95928729487cbda81ebd
SergioHH_22 <sergio.hhidalgo22@gmail.com>
2024-03-13 20:15:42 +0100

    creo el test para las sumas y funciona

--- test/MainTest.java
+++ test/MainTest.java
@@ -8,7 +8,7 @@ class MainTest {
         double sum2=19.897;
         double expResult= 29.897;
         double result= Main.sumar(sum1, sum2);
-        assertEquals(double expResult, double result);
+        assertEquals(expResult, result);
 
     }
 
